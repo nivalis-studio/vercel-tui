a949363bb4792f70c770164ad8f55d7c5c6b4306
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-11-11 00:43:02 +0100

    chore: use zod to parse config and project config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,15 @@
 import fs from 'node:fs';
 import os from 'node:os';
 import path from 'node:path';
+import z from 'zod';
 
-type Config = {
-  bearerToken: string;
-};
+const configSchema = z.object({ bearerToken: z.string().min(1) });
+
+type Config = z.infer<typeof configSchema>;
 
 const CONFIG_DIR = path.join(os.homedir(), '.config', 'lazyvercel');
 const CONFIG_PATH = path.join(CONFIG_DIR, 'config.json');
+const PROJECT_CONFIG_PATH = '.vercel/project.json';
 
 export const getConfig = (): Config | null => {
   try {
@@ -15,7 +17,9 @@ export const getConfig = (): Config | null => {
       return null;
     }
     const content = fs.readFileSync(CONFIG_PATH, 'utf8');
-    return JSON.parse(content) as Config;
+    const config = configSchema.parse(JSON.parse(content) as Config);
+
+    return config;
   } catch (error) {
     console.error('Failed to read config:', error);
     return null;
@@ -34,6 +38,35 @@ export const saveConfig = (config: Config): void => {
   }
 };
 
-export const hasConfig = (): boolean => {
-  return fs.existsSync(CONFIG_PATH);
+const projectConfigSchema = z
+  .object({
+    projectId: z.string().min(1),
+    orgId: z.string().min(1),
+  })
+  .transform(val => ({ projectId: val.projectId, teamId: val.orgId }));
+
+export type ProjectConfig = z.infer<typeof projectConfigSchema>;
+
+export const getProjectConfig = () => {
+  let content: string;
+
+  try {
+    content = fs.readFileSync(PROJECT_CONFIG_PATH, 'utf8');
+  } catch (error) {
+    throw new Error(
+      'Could not read the project config. Try running `vercel link`',
+      { cause: error },
+    );
+  }
+
+  try {
+    const projectConfig = projectConfigSchema.parse(JSON.parse(content));
+
+    return projectConfig;
+  } catch (error) {
+    throw new Error(
+      'Error while parsing the project config. Try running `vercel link`',
+      { cause: error },
+    );
+  }
 };
